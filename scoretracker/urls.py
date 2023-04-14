2f6121711161cac8aab3852ed6730bc5b38abcad
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-04-14 13:50:49 +0000

    Set up backend

---
+++ scoretracker/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+from .views import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('user/signup/', UserCreate.as_view(), name="create_user"),
+    path('users/<int:pk>/', UserDetail.as_view(), name="get_user_details"),
+    path('user/login/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+]
